6c8d2eeda6870ff0330b1d356edde09b4ccc7e98
BlackMagic95 <rohanrk2611@gmail.com>
2026-02-09 21:40:38 +0530

    frontend prod api url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://blockchain-document-verification-1.onrender.com",
   withCredentials: false
 });
 
